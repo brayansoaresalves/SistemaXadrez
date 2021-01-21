df755a384c358a7c03a5e7702f9fef710d11fe06
Brayan Alves <brayan.iub10@gmail.com>
2021-01-20 22:10:31 -0300

    colocando peças no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.piece.King;
+import chess.piece.Rook;
 
 public class ChessMatch { //Partida de xadrez
 	
@@ -8,6 +11,7 @@ public class ChessMatch { //Partida de xadrez
 	
 	public ChessMatch() {
 		board = new Board(8,8);
+		inicialSetup();
 	}
 	
 	// uma matriz de peças de xadrez correspondente a essa partida
@@ -19,6 +23,12 @@ public class ChessMatch { //Partida de xadrez
 			}
 		}
 		return mat;
+	}
+	
+	public void inicialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7,4));
 	}
 
 }
